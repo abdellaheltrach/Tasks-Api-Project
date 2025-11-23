dc3f9a4c1c0d7d2c75184be7c0934e2c2e0780ba
abdellaheltrach <abdellah.eltrach@gmail.com>
2025-11-23 16:12:30 +0100

    update the project to target .NET 10.0

--- LoginApp.DataAccess/obj/Debug/net9.0/LoginApp.DataAccess.AssemblyInfo.cs
+++ LoginApp.DataAccess/obj/Debug/net9.0/LoginApp.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginApp.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30ad069d3fddccab1e80a29ec87df6ad1375ecf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a7321d0daef6a5fe050305dfda7f256858b933")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginApp.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginApp.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
